b3d16526ca923122f7ce0ac76f0ccd3e34cb8767
madmaz038 <151556363+madmaz038@users.noreply.github.com>
2023-11-28 13:18:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function simulateDataAspiration() {
     var progress = 0;
 
     // Utilisez setInterval pour mettre à jour la barre de progression
-    var interval = setInterval(function () {
+    var interval;
+
+    function updateProgressBar() {
         // Mettez à jour la barre de progression jusqu'à 100%
         if (progress < 100) {
-            progress += 5; // Augmentez selon vos besoins, une valeur plus basse accélérera la progression
+            progress += 5; // Augmentez selon vos besoins
             progressBar.style.width = progress + "%";
         } else {
             clearInterval(interval);
@@ -20,7 +22,17 @@ function simulateDataAspiration() {
             // Redirigez l'utilisateur vers la page de données après l'aspiration complète
             redirectToDataPage();
         }
-    }, 80); // Réduisez l'intervalle en millisecondes selon vos besoins pour accélérer la progression
+    }
+
+    interval = setInterval(updateProgressBar, 200); // Réduisez l'intervalle selon vos besoins pour accélérer la progression
+}
+
+function pauseProgressBar() {
+    // Mettez en pause la barre de progression pendant 5 secondes
+    setTimeout(function () {
+        // Reprenez la mise à jour de la barre de progression après la pause
+        simulateDataAspiration();
+    }, 5000); // Pause de 5 secondes (5000 millisecondes)
 }
 
 function redirectToDataPage() {
